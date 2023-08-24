abbfdc34332d97c0a569044c40d4416a79b9e132
Ilham Maulana <93136927+impfundev@users.noreply.github.com>
2023-08-24 13:50:52 +0000

    try optimized next image

--- blog/components/Card.js
+++ blog/components/Card.js
@@ -6,7 +6,10 @@ export default function Card({ title, image, url, width, heigh }) {
     <Link href={`/${url}`} className="flex flex-col gap-4">
       <Image
         className="w-full h-auto object-cover rounded-2xl"
+        priority={true}
+        quality={50}
         src={image}
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         alt={title}
         width={width}
         height={heigh}
--- blog/next.config.js
+++ blog/next.config.js
@@ -3,6 +3,8 @@
 module.exports = {
   images: {
     formats: ["image/avif", "image/webp"],
+    deviceSizes: [320, 375, 425, 768, 1024, 1440, 2560],
+    imageSizes: [16, 32, 37, 42, 76, 102, 144, 256],
     remotePatterns: [
       {
         protocol: "https",
