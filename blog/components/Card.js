eb0cf80ec7b7af219c140920017a4256b83a3241
Ilham Maulana <93136927+impfundev@users.noreply.github.com>
2023-08-24 14:06:14 +0000

    setup conditional image to load lazy or eager

--- blog/components/Card.js
+++ blog/components/Card.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export default function Card({ title, image, url, width, heigh }) {
+export default function Card({ title, image, url, width, heigh, lazy }) {
   return (
     <Link href={`/${url}`} className="flex flex-col gap-4">
       <Image
@@ -13,6 +13,7 @@ export default function Card({ title, image, url, width, heigh }) {
         alt={title}
         width={width}
         height={heigh}
+        loading={lazy ? "lazy" : "eager"}
       />
       <h2 className="card-header">{title}</h2>
     </Link>
